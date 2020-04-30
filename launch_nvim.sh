6eedb897d2a613dc36ad14149cbf515db0622544
Christian Günther <christianr.guenther@gmail.com>
2020-04-30 07:21:50 +0200

    Adaptations wrt. to new dotfiles symlink

--- launch_nvim.sh
+++ launch_nvim.sh
@@ -2,8 +2,7 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $dir && \
-    exec ../bin/nvim -S ${dir}/../.config/nvim/last_session.vim
+cd $dir && exec $HOME/bin/nvim -S $HOME/.config/nvim/last_session.vim
 
 exit 0
 
