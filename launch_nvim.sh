48eea4e5bc9b8d11a8e79f0680ebbb773177123a
Christian Günther <christianr.guenther@gmail.com>
2020-05-04 13:00:15 +0200

    Renamed: ~/.config/nvim/last_session.vim -> ~/workspace/nvim__last_session.vim

--- launch_nvim.sh
+++ launch_nvim.sh
@@ -3,7 +3,8 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # cd $dir && exec $HOME/bin/nvim -S $HOME/.config/nvim/last_session.vim
-cd $dir && exec nvim -S $HOME/.config/nvim/last_session.vim
+# cd $dir && exec nvim -S $HOME/.config/nvim/last_session.vim
+cd $dir && exec nvim -S $HOME/workspace/nvim__last_session.vim
 
 exit 0
 
