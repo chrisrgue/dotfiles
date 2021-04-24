8c1b3b1f39625fbc500d00790208b0e5648884b8
Christian Günther <christianr.guenther@gmail.com>
2021-04-24 18:15:53 +0000

    Minor bugfixes

--- bin/list_cloned_repos.sh
+++ bin/list_cloned_repos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for d in $(ls -d ~/github_repos/*);do
+for d in $(ls -d -- ~/github_repos/*/);do
     echo "git clone $(cd $d && git remote -v | grep '(fetch)' | awk '{print $2}') $(basename $d)"
 done
